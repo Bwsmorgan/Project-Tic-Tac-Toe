14d017b18fa49e7e89078822a0964e5c3ab03df9
Brendan Morgan <bmo_32@hotmail.com>
2023-05-10 20:28:04 -0400

    Add new ruby files board.rb game.rb and player.rb

---
+++ game.rb
@@ -0,0 +1,22 @@
+require_relative 'player.rb'
+class Game
+
+    def play
+
+        puts "\nWELCOME TO RUBY TIC TAC TOE"
+        puts "---------------------------"
+
+        @player1 = Player.new
+
+
+
+
+    end
+
+
+
+end
+
+game1 = Game.new()
+
+game1.play()
